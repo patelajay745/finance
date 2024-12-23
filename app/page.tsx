ab08a19755d59a801c48664f9f085734ce18d31e
Ajay patel <patel.ajay745@gmail.com>
2024-12-22 20:46:31 -0500

    added how it works

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/hero";
 import { Card, CardContent } from "@/components/ui/card";
-import { featuresData, statsData } from "@/data/landing";
+import { featuresData, howItWorksData, statsData } from "@/data/landing";
 
 export default function Home() {
   return (
@@ -36,6 +36,23 @@ export default function Home() {
                   <p className="text-gray-600">{feature.description}</p>
                 </CardContent>
               </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-blue-50 ">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-16">How It Works</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {howItWorksData?.map((step, index) => (
+              <div key={index} className="text-center">
+                <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-6">
+                  {step.icon}
+                </div>
+                <h3 className="text-xl font-semibold mb-4">{step.title}</h3>
+                <p className="text-gray-600">{step.description}</p>
+              </div>
             ))}
           </div>
         </div>
