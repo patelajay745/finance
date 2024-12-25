c843e530607cf782f37364c5329e8a64e33ce61d
Ajay patel <patel.ajay745@gmail.com>
2024-12-24 23:56:42 -0500

    added join now

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   statsData,
   testimonialsData,
 } from "@/data/landing";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -74,6 +75,28 @@ export default function Home() {
             autoplay={true}
             testimonials={testimonialsData}
           />
+        </div>
+      </section>
+
+      <section className="py-20  bg-blue-600">
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold text-center mb-4 text-white">
+            Ready to Take Control of your Finances?
+          </h2>
+
+          <p className="text-blue-100 mb-8 max-w-3xl mx-auto">
+            Join thousands of users who are already managing their finances
+            smart with Pocket planner
+          </p>
+
+          <Link href="/dashboard">
+            <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-white px-3 py-1 text-sm font-medium text-blue-600 backdrop-blur-3xl">
+                Start free trial
+              </span>
+            </button>
+          </Link>
         </div>
       </section>
     </div>
