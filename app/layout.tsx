47147a84ecb6712ad2323bcabdb6cb334606ca89
Ajay patel <patel.ajay745@gmail.com>
2024-12-25 00:33:10 -0500

    removed unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
