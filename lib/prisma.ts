e112b93df20f8b5ba62f8b8ffbbfaf673a4a75ea
Ajay patel <patel.ajay745@gmail.com>
2024-12-28 08:58:27 -0500

    solved prisma singleton error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
 }
-
-export const db = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = db;
